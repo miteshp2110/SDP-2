ea164eb7ad169b0017d0b262e106917f81dff400
Gaurav-Khivasara <gkgauravgk123@gmail.com>
2024-03-10 03:37:36 +0530

    notifications

--- TaskManagement/MyProject/MyApp/models.py
+++ TaskManagement/MyProject/MyApp/models.py
@@ -3,10 +3,9 @@ from django.db import models
 # Create your models here.
 
 class UserData(models.Model):
-    email=models.EmailField(blank=False,default="123@gmail.com")
+    email=models.EmailField(blank=False)
     name=models.CharField(blank=False,max_length=150)
     password=models.CharField(blank=False,max_length=150)
-    sessionId=models.CharField(blank=True,max_length=150)
 
     class Meta:
         db_table="Users"
