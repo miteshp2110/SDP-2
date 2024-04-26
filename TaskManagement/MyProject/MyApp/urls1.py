9e2159f99967809189bc7b6271f9376b96684b63
miteshp2110 <paliwalmitesh2110@gmail.com>
2024-04-26 14:18:31 +0530

    Added small changes

--- TaskManagement/MyProject/MyApp/urls1.py
+++ TaskManagement/MyProject/MyApp/urls1.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import adminViews
 urlpatterns = [
     path('',adminViews.home,name='adminHome'),
-    path('accountAdmin/',adminViews.account,name='account'),
+    path('accountAdmin/',adminViews.account,name='adminAccount'),
     path('accountAdmin/logout',adminViews.logout),
     path('accountAdmin/login',adminViews.login)
 
